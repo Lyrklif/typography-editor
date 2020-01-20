30cb33029452ba025cb3e0741244aebb3622115d
Lyrklif <kochurow121@gmail.com>
2020-01-20 15:54:02 +0400

    build

--- build/precache-manifest.5b012ffc67d3b09f23efe46d9c59bafd.js
+++ build/precache-manifest.5b012ffc67d3b09f23efe46d9c59bafd.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "873dd253d2f3e3941792b0168d3eb3e1",
+    "revision": "35f0571f60762bab25bacb35b1f4b5b7",
     "url": "./index.html"
   },
   {
-    "revision": "498a93a00e9be61be509",
+    "revision": "20db5f99e75e3f08eb03",
     "url": "./static/css/main.decd4050.chunk.css"
   },
   {
-    "revision": "02d152971db90ea88880",
-    "url": "./static/js/2.66ca8dd0.chunk.js"
+    "revision": "2796f5a9a1067ca3fffc",
+    "url": "./static/js/2.60c89ba0.chunk.js"
   },
   {
     "revision": "1d18f4c3de57cbb9bac2a2f25e0142a0",
-    "url": "./static/js/2.66ca8dd0.chunk.js.LICENSE"
+    "url": "./static/js/2.60c89ba0.chunk.js.LICENSE"
   },
   {
-    "revision": "498a93a00e9be61be509",
-    "url": "./static/js/main.f70e4ed4.chunk.js"
+    "revision": "20db5f99e75e3f08eb03",
+    "url": "./static/js/main.75ace550.chunk.js"
   },
   {
     "revision": "1c20036138a5e7d0b4d4",
