e3e5a242f3705357ada048b96bd632837df1956d
Alexandr Kochurov <b2b4ly@gmail.com>
2022-11-05 21:47:09 +0600

    Updates

---
+++ precache-manifest.59cf5dd7221e5c610e7ec068dbab8949.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6be2e33595c8c82dd92b6e5dcaa0e050",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fda5beec69abfca2b871",
+    "url": "./static/css/main.910b0299.chunk.css"
+  },
+  {
+    "revision": "ae2e75535bda0eb12a0e",
+    "url": "./static/js/2.79a7e9e1.chunk.js"
+  },
+  {
+    "revision": "3d9f73c87cd93d572198204d582856ba",
+    "url": "./static/js/2.79a7e9e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fda5beec69abfca2b871",
+    "url": "./static/js/main.f6f9d4ba.chunk.js"
+  },
+  {
+    "revision": "1c20036138a5e7d0b4d4",
+    "url": "./static/js/runtime-main.32edbf5e.js"
+  }
+]);
